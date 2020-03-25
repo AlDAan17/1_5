e3db4ad19240a130b3a6a2bc04ad37044067b556
Aldani <kototi17@gmail.com>
2020-03-25 21:50:28 +0300

    test2

---
+++ app.js
@@ -0,0 +1,8 @@
+// import Swiper from '/swiper.js'
+// if (document.body.clientWidth < 768) { //вот тут
+//     let mySwiper_technics = new Swiper('.swiper-container', {
+//         pagination: {
+//             el: '.swiper-pagination',
+//             slidesPerView: 'auto'
+//         }
+//     };
