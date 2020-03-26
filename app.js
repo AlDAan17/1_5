d0b8890938729f5d8e39d91fa7565d66cb6b85b6
Aldani <kototi17@gmail.com>
2020-03-26 18:47:46 +0300

    test2

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-// import Swiper from '/swiper.js'
-// if (document.body.clientWidth < 768) { //вот тут
-//     let mySwiper_technics = new Swiper('.swiper-container', {
-//         pagination: {
-//             el: '.swiper-pagination',
-//             slidesPerView: 'auto'
-//         }
-//     };
+if (document.body.clientWidth < 768) {
+    let mySwiper_technics = new Swiper('.swiper-container', {
+        pagination: {
+            el: '.swiper-pagination',
+            slidesPerView: 'auto',
+        }
+    });
+}
+
+function showMore() {
+    let viewsonic = document.getElementById("slide-viewsonic");
+    let sony = document.getElementById("slide-sony");
+    let lenovo = document.getElementById("slide-lenovo");
+    let samsung = document.getElementById("slide-samsung");
+    let apple = document.getElementById("slide-apple");
+    let btnMore = document.getElementById("btn-more");
+    let btnShowMore = document.getElementById("swiper-shwomore");
+
+    if (viewsonic.style.display === "none"){
+        viewsonic.style.display = "flex";
+    }else {
+        viewsonic.style.display = "flex";
+        btnShowMore.innerHTML = "Read less";
+    }
+}
